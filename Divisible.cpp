f6c31fc992dbda076ea329ce8502eee05cb60b4f
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:30:49 -0500

    Create Divisible.cpp

---
+++ Divisible.cpp
@@ -0,0 +1,14 @@
+package Clase2;
+
+public class Divisible {
+	
+	public static void main(String [] args ) {
+		int numero1=5, numero2=5; //Solo_cambia_los_variables_que_desees.
+		
+		if(numero1%numero2 == 0){
+			System.out.println("El resto de la division es:"+ numero1%numero2);
+			System.out.println("El numero es divisible");	
+		}
+		System.out.println("El resto de la division es:"+ numero1%numero2);
+	}
+}
