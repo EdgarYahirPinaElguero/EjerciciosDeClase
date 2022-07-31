9ce23c3b95552de31f55dd007c242bcee8d4b182
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:41:43 -0500

    Create CualEsElValor.cpp

---
+++ CualEsElValor.cpp
@@ -0,0 +1,18 @@
+package Clase1;
+import java.util.*;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+	
+	int numero1; 
+	int numero2;
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Intoroduzca el primer nunmero");
+	numero1 = sc.nextInt();
+	System.out.println("Intoroduzca el segundo nunmero");
+	numero2 = sc.nextInt();
+	
+	System.out.println("El primer numero es: "+numero1+" y el segundo numero es: "+numero2);
+	}
+}
